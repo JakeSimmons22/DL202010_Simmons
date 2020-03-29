d25dc12288876c2f778e4ab59641507cddc3ac94
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-03-29 13:08:12 -0500

    save

---
+++ Lab9_ALU_2/Lab9_ALU_2.srcs/sim_1/new/ALU_Test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2020 01:07:15 PM
+// Design Name: 
+// Module Name: ALU_Test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_Test(
+
+    );
+endmodule
---
+++ Lab9_ALU_2/Lab9_ALU_2.srcs/sim_2/new/ALU_Test.sv
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ps
+//ELC 2137 Jake Simmons 2020-3-29
+
+module ALU_Test();
+
+endmodule
