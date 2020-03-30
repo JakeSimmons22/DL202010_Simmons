9f93d6c54b8ce89b467bc8790e2e6b32e9bee816
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-03-30 16:57:14 -0500

    save

--- Lab9_ALU_2/Lab9_ALU_2.srcs/sources_1/new/top_lab9.sv
+++ Lab9_ALU_2/Lab9_ALU_2.srcs/sources_1/new/top_lab9.sv
@@ -13,14 +13,14 @@ module top_lab9(
         wire [7:0] W1;
         wire [7:0] W2;
         
-        register_2 r1( .D(sw[7:0]), .en(btnD), .clk(clk), .Q(W1), .rst(btnC)
+        register_2 #(.N(8)) r1( .D(sw[7:0]), .en(btnD), .clk(clk), .Q(W1), .rst(btnC)
        );
        
        ALU alu( .in1(W1), .in0(sw[7:0]), .op(sw[11:8]), .out(W2)
        
        );
        
-       register_2 r2( .D(W2), .en(btnU), .clk(clk), .rst(btnC), .Q(led[15:8])
+       register_2 #(.N(8)) r2( .D(W2), .en(btnU), .clk(clk), .rst(btnC), .Q(led[15:8])
        
        );
        
