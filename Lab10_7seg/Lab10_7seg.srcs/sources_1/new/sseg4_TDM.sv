ccb16e3c6889952bb0c5c91efdb1b2281648c955
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-07 16:33:48 -0500

    save

--- Lab10_7seg/Lab10_7seg.srcs/sources_1/new/sseg4_TDM.sv
+++ Lab10_7seg/Lab10_7seg.srcs/sources_1/new/sseg4_TDM.sv
@@ -1,26 +1,50 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/07/2020 04:30:20 PM
-// Design Name: 
-// Module Name: sseg4_TDM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+//ELC 2137 2020-4-7
 
 module sseg4_TDM(
+     input [15:0] data,
+
+    input hex_dec,
+
+    input sign,
+
+    input [1:0] digit_sel,
+
+    output [6:0] seg,
+
+    output dp,
+
+    output [3:0] an
 
     );
+
+    wire [15:0] W1 ;
+
+    wire [15:0] W2 ;
+
+    wire [3:0] W3;
+
+    wire [6:0] W4;
+
+    wire [3:0] W5;
+
+    wire W6;
+
+    BCD11_2 B1( .in11(data[10:0]), .out11(W1));
+
+    mux2 #(.N(16)) B2( .in0(W1), .in1(data), .sel(hex_dec), .out(W2));
+
+    mux4 B3( .in0(W2[3:0]), .in1(W2[7:4]), .in2(W2[11:8]), .in3(W2[15:12]), .sel(digit_sel), .out(W3));
+
+    sseg_decoder B5( .num(W3), .sseg(W4));
+
+    mux2 #(.N(7)) B6( .in0(W4), .in1(7'b0111111), .out(seg) , .sel(W6));
+
+    and G2( W6, sign, ~W5[3]);
+
+    annode_decoder B7( .in(digit_sel), .out(W5));
+
+    assign dp = 1;
+
+    assign an = W5;
 endmodule
