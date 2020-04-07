990742896e9a3ba18e8e095ddd3245717f805f30
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-07 15:52:08 -0500

    save

---
+++ Lab10_7seg/Lab10_7seg.srcs/sim_1/new/counter_test.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2020 03:51:59 PM
+// Design Name: 
+// Module Name: counter_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter_test(
+
+    );
+endmodule
