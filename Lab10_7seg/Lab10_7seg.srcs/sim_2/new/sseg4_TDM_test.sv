49206d887b33aad84281187f7eaad74cdf4866bd
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-08 11:43:18 -0500

    save

--- Lab10_7seg/Lab10_7seg.srcs/sim_2/new/sseg4_TDM_test.sv
+++ Lab10_7seg/Lab10_7seg.srcs/sim_2/new/sseg4_TDM_test.sv
@@ -35,20 +35,20 @@ end
 
 initial begin
 
-    data = 0; hex_dec = 0; reset = 1; clock = 18'b000000000000000000; sign = 0; #10;
-    data = 1; hex_dec = 1; reset = 0; clock = 18'b000000000000000001; sign = 0; #10;
-    data = 2; hex_dec = 1; reset = 0; clock = 18'b000000000000000010; sign = 0; #10;
-    data = 3; hex_dec = 1; reset = 0; clock = 18'b000000000000000011; sign = 0; #10;
-    data = 4; hex_dec = 1; reset = 0; clock = 18'b000000000000000100; sign = 0; #10;
-    data = 5; hex_dec = 1; reset = 0; clock = 18'b000000000000000101; sign = 0; #10;
-    data = 6; hex_dec = 1; reset = 0; clock = 18'b000000000000000110; sign = 0; #10;
-    data = 7; hex_dec = 1; reset = 0; clock = 18'b000000000000000111; sign = 0; #10;
-    data = 8; hex_dec = 1; reset = 0; clock = 18'b000000000000001000; sign = 0; #10;
-    data = 9; hex_dec = 1; reset = 0; clock = 18'b000000000000001001; sign = 0; #10;
-    data = 10; hex_dec = 1; reset = 0; clock = 18'b000000000000001010; sign = 0; #10;
-    data = 11; hex_dec = 1; reset = 0; clock = 18'b000000000000001011; sign = 0; #10;
-    data = 12; hex_dec = 1; reset = 0; clock = 18'b000000000000001100; sign = 0; #10;
-    data = 13; hex_dec = 1; reset = 0; clock = 18'b000000000000001101; sign = 0; #10;
+    data = 0; hex_dec = 0; reset = 1; clock = 18'b000000000000000000; sign = 0; #2621440;
+    data = 1; hex_dec = 1; reset = 0; clock = 18'b000000000000000001; sign = 0; #2621440;
+    data = 2; hex_dec = 1; reset = 0; clock = 18'b000000000000000010; sign = 0;  #2621440;
+    data = 3; hex_dec = 1; reset = 0; clock = 18'b000000000000000011; sign = 0;  #2621440;
+    data = 4; hex_dec = 1; reset = 0; clock = 18'b000000000000000100; sign = 0;  #2621440;
+    data = 5; hex_dec = 1; reset = 0; clock = 18'b000000000000000101; sign = 0;  #2621440;
+    data = 6; hex_dec = 1; reset = 0; clock = 18'b000000000000000110; sign = 0;  #2621440;
+    data = 7; hex_dec = 1; reset = 0; clock = 18'b000000000000000111; sign = 0;  #2621440;
+    data = 8; hex_dec = 1; reset = 0; clock = 18'b000000000000001000; sign = 0;  #2621440;
+    data = 9; hex_dec = 1; reset = 0; clock = 18'b000000000000001001; sign = 0;  #2621440;
+    data = 10; hex_dec = 1; reset = 0; clock = 18'b000000000000001010; sign = 0;  #2621440;
+    data = 11; hex_dec = 1; reset = 0; clock = 18'b000000000000001011; sign = 0;  #2621440;
+    data = 12; hex_dec = 1; reset = 0; clock = 18'b000000000000001100; sign = 0;  #2621440;
+    data = 13; hex_dec = 1; reset = 0; clock = 18'b000000000000001101; sign = 0;  #2621440;
 
 
       $finish;
