206e87b9e72b24b7e0860f70ac81b6ea166fe1b5
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-07 21:38:53 -0500

    save

--- Lab10_7seg/Lab10_7seg.srcs/sim_1/new/sseg4_TDM_test.sv
+++ Lab10_7seg/Lab10_7seg.srcs/sim_1/new/sseg4_TDM_test.sv
@@ -1,26 +1,40 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/07/2020 04:50:43 PM
-// Design Name: 
-// Module Name: sseg4_TDM_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//ELC 2137 2020-7-4
+
+module sseg4_TDM_test();
+    reg clock;
+    reg reset;
+    reg [15:0] data;
+    reg hex_dec;
+    reg sign;
+    wire [6:0] seg;
+    wire dp;
+    wire [3:0] an;
+
+sseg4_TDM sseg4( .clock(clock), .reset(reset), .data(data), .hex_dec(hex_dec),
+    .sign(sign), .seg(seg), .dp(dp), .an(an) );
+
+    
+
+// clock runs continuously 
+
+always begin 
+
+    clock = ~clock; #2621440; 
+
+end
+
+// this block only runs once 
+
+initial begin
+
+    data = 0; hex_dec = 1; reset = 0; clock = 0; sign = 0; #10;
+    data = 1; hex_dec = 1; reset = 0; clock = 1; sign = 0; #10;
+    data = 2; hex_dec = 1; reset = 0; clock = 2; sign = 0; #10;
+    data = 3; hex_dec = 1; reset = 0; clock = 100; sign = 1; #10;
 
 
-module sseg4_TDM_test(
+      $finish;
 
-    );
+    end
 endmodule
