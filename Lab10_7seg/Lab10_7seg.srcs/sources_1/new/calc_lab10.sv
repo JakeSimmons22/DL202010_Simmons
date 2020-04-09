d60e8b6dc4a1e9a3548e9cb6aaf52acb6a2ff772
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-09 17:55:55 -0500

    save

--- Lab10_7seg/Lab10_7seg.srcs/sources_1/new/calc_lab10.sv
+++ Lab10_7seg/Lab10_7seg.srcs/sources_1/new/calc_lab10.sv
@@ -2,11 +2,10 @@
 //ELC 2137 Jake Simmons 2020-4-8
 
 module calc_lab10(
-    input [17:0] clock,
+    input [17:0] clk,
     input btnU,
     input btnD,
     input [15:0] sw,
-    input clk,
     input btnC,
     output [15:0] led,
     output [6:0] seg,
@@ -17,7 +16,7 @@ module calc_lab10(
     wire [7:0] W2;
     
     sseg4_TDM disp_unit( .data({W2, 8'b00000000}), .hex_dec(sw[15]),
-     .reset(btnC), .clock(clock), .sign(sw[14]), 
+     .reset(btnC), .clock(clk), .sign(sw[14]), 
      .seg(seg), .dp(dp), .an(an));
     
     top_lab9 calc_unit( .btnU(btnU), .btnD(btnD), .sw(sw[11:0]),
