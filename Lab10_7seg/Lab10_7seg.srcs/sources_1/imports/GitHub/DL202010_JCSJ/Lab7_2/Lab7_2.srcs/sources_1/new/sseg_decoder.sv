25b76b323258408611f2f7db7308f547cee8d008
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-13 18:03:39 -0500

    save

--- Lab10_7seg/Lab10_7seg.srcs/sources_1/imports/GitHub/DL202010_JCSJ/Lab7_2/Lab7_2.srcs/sources_1/new/sseg_decoder.sv
+++ Lab10_7seg/Lab10_7seg.srcs/sources_1/imports/GitHub/DL202010_JCSJ/Lab7_2/Lab7_2.srcs/sources_1/new/sseg_decoder.sv
@@ -8,7 +8,7 @@ module sseg_decoder(
     always @ *
         case ( num )
         4'h0: sseg = 7'b1000000;
-        4'h1: sseg = 7'b1001111;
+        4'h1: sseg = 7'b1111001;
         4'h2: sseg = 7'b0100100;
         4'h3: sseg = 7'b0110000;
         4'h4: sseg = 7'b0011001;
@@ -16,7 +16,7 @@ module sseg_decoder(
         4'h6: sseg = 7'b0000010;
         4'h7: sseg = 7'b1111000;
         4'h8: sseg = 7'b0000000;
-        4'h9: sseg = 7'b0011000;
+        4'h9: sseg = 7'b0110000;
         4'hA: sseg = 7'b0001000;
         4'hB: sseg = 7'b0000011;
         4'hC: sseg = 7'b1000110;
