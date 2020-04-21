1cf0104db73b7af3333554e81e43c2d79bc9d0c4
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-21 16:01:43 -0500

    save

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -2,13 +2,13 @@
 //ELC 2137, Jake Simmons, 2020-04-20
 
 
-module guess_FSM (
+module guess_FSM #(parameter N=19)(
     input [3:0]b,
     input reset,
     input clk,
     output reg [3:0]y,
-    output win,
-    output lose
+    output reg win,
+    output reg lose
     );
     
     
@@ -22,32 +22,77 @@ module guess_FSM (
         SLOSE = 3'b101;
         
     //internal signals
-    reg[2:0] nState;
-
+    reg[2:0] nState, State;
+    reg[N-1:0] counter, ncounter;
     
+    always_ff @(posedge clk or posedge reset)
+        if(reset) begin
+            State <= S0;
+            counter <= {N{1'b1}};
+        end
+        else begin
+            State <= nState;
+            counter <= ncounter;
+        end
+        
+
+ always_comb begin
     case(State)
         S0: begin
-           y[0] = 1'b1;
-           if(b==1)
-            nState = SWIN;
-            
-    
-
-always @*
-    begin
-        //state zero
-        if(~b & state == 2'b11) 
-            assign y[0] = 1'b1;
-        else if(~b & swin == 1'b1)
-            assign y[0] = 1'b1;
-        else(~b & slose == 1'b1)
-            assign y[0] = 1'b1;
-       //state one
-       //state two
-       //state three
-   end
+            y[0] = 1'b1;
+            if(b==1)
+                nState = SWIN;
+            else if(b==0)
+                nState = S1;
+            else 
+                nState = SLOSE;
+        end
         
-         
-    
-    
+        S1: begin
+            y[1] = 1;
+            if(b== 2)
+                nState = SWIN;
+            else if(b==0)
+                nState = S2;
+            else
+                nState = lose;
+        end
+        
+        S2: begin
+            y[2] = 1;
+            if(b==4)
+                nState = SWIN;
+            else if(b==0)
+                nState = S3;
+            else
+                nState = SLOSE;
+        end
+        
+        S3: begin
+            y[3] = 1;
+            if(b==8)
+                nState = SWIN;
+            else if(b==0)
+                nState = S0;
+            else
+                nState = SLOSE;
+        end
+        
+        SWIN: begin
+            win = 1;
+            if(b==0)
+                nState = S0;
+            else
+                nState = SWIN;
+        end
+        
+        SLOSE: begin
+            lose = 1;
+            if(b==0)
+                nState = S0;
+            else
+                nState = SLOSE;
+        end
+   endcase
+ end   
 endmodule
