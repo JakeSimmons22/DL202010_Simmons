1d76c122c33fd53c262e9a7ed914219c09c6eb79
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-21 15:42:42 -0500

    save

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -6,12 +6,32 @@ module guess_FSM (
     input [3:0]b,
     input reset,
     input clk,
-    output [3:0]y,
+    output reg [3:0]y,
     output win,
     output lose
-);
-    reg[1:0] state;
-    reg swin, slose;
+    );
+    
+    
+    //states
+    localparam [2:0]
+        S0 = 3'b000,
+        S1 = 3'b001,
+        S2 = 3'b010,
+        S3 = 3'b011,
+        SWIN = 3'b100,
+        SLOSE = 3'b101;
+        
+    //internal signals
+    reg[2:0] nState;
+
+    
+    case(State)
+        S0: begin
+           y[0] = 1'b1;
+           if(b==1)
+            nState = SWIN;
+            
+    
 
 always @*
     begin
