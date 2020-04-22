45a716f3e8613664ac52849ed0ed20a7daafba73
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-22 13:20:11 -0500

    save

---
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guessing_game.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/22/2020 01:18:46 PM
+// Design Name: 
+// Module Name: guessing_game
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module guessing_game(
+
+    );
+endmodule
