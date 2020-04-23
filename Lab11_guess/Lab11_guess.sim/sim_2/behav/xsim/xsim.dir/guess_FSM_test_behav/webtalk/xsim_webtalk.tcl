2c49ea391457de8ab13f17612989b971aa050a09
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-23 11:02:26 -0500

    save

--- Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/xsim_webtalk.tcl
+++ Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Apr 23 09:56:52 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Apr 23 10:45:32 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "2d4b7607-c373-4c2c-98fe-e1008671258c" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "75e8831915cb4135874b7c61b14a58c8" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "27" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "29" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8550U CPU @ 1.80GHz" -context "user_environment"
@@ -37,7 +37,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "23" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "3" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip unimacro_ver unisims_ver " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "1.80_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "39400_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4174741928 -regid "" -xml C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "1.92_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "39380_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2560394839 -regid "" -xml C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/School/Documents/GitHub/DL202010_Simmons/Lab11_guess/Lab11_guess.sim/sim_2/behav/xsim/xsim.dir/guess_FSM_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
