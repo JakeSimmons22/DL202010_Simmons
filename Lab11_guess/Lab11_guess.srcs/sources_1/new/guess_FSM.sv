1a12021abad754d0240dbe221255bf9e6ef3ebf4
Jake Simmons <59978822+JakeSimmons22@users.noreply.github.com>
2020-04-23 09:08:47 -0500

    save

--- Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
+++ Lab11_guess/Lab11_guess.srcs/sources_1/new/guess_FSM.sv
@@ -48,20 +48,20 @@ module guess_FSM (
         
         S1: begin
             y[1] = 1;
-            //y[0] = 0;
-            //y[3:2] = 0;
+            y[0] = 0;
+            y[3:2] = 0;
             if(b==2)
                 nState = SWIN;
             else if(b==0)
                 nState = S2;
             else 
-                nState = lose;
+                nState = SLOSE;
         end
         
         S2: begin
             y[2] = 1;
-            //y[3] = 0;
-            //y[1:0] = 0;
+            y[3] = 0;
+            y[1:0] = 0;
             if(b==4)
                 nState = SWIN;
             else if(b==0)
@@ -72,7 +72,7 @@ module guess_FSM (
         
         S3: begin
             y[3] = 1;
-            //y[2:0] = 0;
+            y[2:0] = 0;
             if(b==8)
                 nState = SWIN;
             else if(b==0)
